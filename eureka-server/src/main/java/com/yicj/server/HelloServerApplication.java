9402c256957051e682b2ff04742b5b0145429577
易成杰 <626659321@qq.com>
2023-06-14 22:40:58 +0800

    部分更新

---
+++ eureka-server/src/main/java/com/yicj/server/HelloServerApplication.java
@@ -0,0 +1,17 @@
+package com.yicj.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: yicj
+ * @date: 2023/6/14 22:39
+ */
+@SpringBootApplication
+public class HelloServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloServerApplication.class) ;
+    }
+}
